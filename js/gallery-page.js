db489d5d79a2b902fc594397fd14e92fefd1457a
Barbod Garsha <barbodgarshasbi79@gmail.com>
2022-03-05 19:48:55 +0100

    search results are now clickable

--- js/gallery-page.js
+++ js/gallery-page.js
@@ -1,3 +1,5 @@
+//TEMPLATES
+const results_template = document.getElementById('results-template')
 
 const search_bar__input = document.querySelector('[data-search-bar__input]')
 const search_bar__recs = document.querySelector('[data-search-bar__recommendations]')
@@ -11,8 +13,10 @@ const search_bar__recs__conatiner = document.querySelector('[data-search-bar__re
 //TESTING...
 let tags = [{id: 'CATS', name: 'Cats'}, {id: 'DOGS', name: 'Dogs'}]
 
-
 //EVENTS
+search_bar__recs.addEventListener('mousedown', function(e){
+    if(e.target.hasAttribute('data-search-bar__recommendations__item')) { console.log(e.target.id)}
+})
 
 search_bar__input.addEventListener('focus', function(e){
     search_bar__recs.style.display = 'block'
@@ -45,14 +49,14 @@ function render_search_results(filter) {
         
         for (i = 0; i < tags.length; i++) {
             if (tags[i].name.toUpperCase().indexOf(filter) > -1) {
-                const result = document.createElement('p')
-                result.dataset.listId = tags[i].id
-                result.classList.add('search-bar__recommendations__item')
-                result.innerText = tags[i].name
-                search_bar__recs__conatiner.appendChild(result)
                 
+                const result = document.importNode(results_template.content, true)
+                const result_p = result.querySelector('[data-search-bar__recommendations__item]')   
+                result_p.id = tags[i].id
+                result_p.innerText = tags[i].name
+                search_bar__recs__conatiner.appendChild(result)
+
                 found_smth = true
-            } else {
             }
         }
 
@@ -61,11 +65,11 @@ function render_search_results(filter) {
     }
     else {
         for (i = 0; i < tags.length; i++) {
-                const result = document.createElement('p')
-                result.dataset.listId = tags[i].id
-                result.classList.add('search-bar__recommendations__item')
-                result.innerText = tags[i].name
-                search_bar__recs__conatiner.appendChild(result)
+            const result = document.importNode(results_template.content, true)
+            const result_p = result.querySelector('[data-search-bar__recommendations__item]')   
+            result_p.id = tags[i].id
+            result_p.innerText = tags[i].name
+            search_bar__recs__conatiner.appendChild(result)
         }
     }
 }
