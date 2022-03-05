3d92fbdec2e7caa33e4a9a4598ea1cd01812f35c
Barbod Garsha <barbodgarshasbi79@gmail.com>
2022-03-05 17:23:20 +0100

    the go-to-gallery-btn is functional now

---
+++ js/homepage.js
@@ -0,0 +1,5 @@
+const go_to_gallery_btn = document.querySelector('[data-go-to-gallery-btn]')
+
+go_to_gallery_btn.addEventListener('click', function(e) {
+    window.location.href = 'gallery-page.html'
+})
